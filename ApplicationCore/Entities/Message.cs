479216d804db418b40199cadad402500a33051ef
h-mitsuzono <h-mitsuzono@alterbooth.com>
2021-07-28 14:23:07 +0900

    Create project

---
+++ ApplicationCore/Entities/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ApplicationCore.Entities
+{
+    /// <summary>
+    /// 返信メッセージ
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        Guid Id { get; set; }
+
+        /// <summary>
+        /// テキスト
+        /// </summary>
+        string Text { get; set; }
+
+        // TODO 「返信条件（閾値）」や「テンプレート・Flexメッセージに対応したパラメータ」等を必要に応じて追加
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# hol-azure-line-bot
