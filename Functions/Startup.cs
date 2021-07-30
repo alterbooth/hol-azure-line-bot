ff30eabb9960b9e760a6a9e7c327b35277b10ce8
h-mitsuzono <h-mitsuzono@alterbooth.com>
2021-07-30 18:04:07 +0900

    Add Functions project

---
+++ Functions/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(Functions.Startup))]
+namespace Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient("line", c => c.BaseAddress = new Uri("https://api.line.me"));
+        }
+    }
+}
