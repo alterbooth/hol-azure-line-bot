054a5109dec495a09025ec3aa37429d1e6decee4
HironoBaba <h-baba@alterbooth.com>
2022-12-12 14:25:52 +0900

    [modify][delete] Functionsのみのプロジェクトに再編集

---
+++ Functions/Entities/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ApplicationCore.Entities
+{
+    /// <summary>
+    /// 返信メッセージ
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// テキスト
+        /// </summary>
+        public string Text { get; set; }
+
+        // TODO 「返信条件（閾値）」や「テンプレート・Flexメッセージに対応したパラメータ」等を必要に応じて追加
+    }
+}
